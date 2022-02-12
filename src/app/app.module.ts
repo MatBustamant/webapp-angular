2006536f54ef664be172057bb4a97e3e56495d39
MatBustamant <matexequiel.2012@gmail.com>
2022-02-12 12:28:05 -0300

    top-bar y menu desplegable optimizados un poco para PC

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
 import { TopBarComponent } from './components/top-bar/top-bar.component';
-import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { CollapsableNavbarComponent } from './components/collapsable-navbar/collapsable-navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    HeaderComponent,
     TopBarComponent,
-    NavBarComponent
+    CollapsableNavbarComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/collapsable-navbar/collapsable-navbar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-collapsable-navbar',
+  templateUrl: './collapsable-navbar.component.html',
+  styleUrls: ['./collapsable-navbar.component.css']
+})
+export class CollapsableNavbarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
